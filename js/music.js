d419aaccffa4c7eae75a3c8838ef67882411cc4b
anjali-1607 <anjalirajpoot1609@gmail.com>
2024-02-03 14:09:33 +0530

    adjust photo and heading

--- js/music.js
+++ js/music.js
@@ -3,3 +3,22 @@ a = a.filter(function(x) {
     return x>-1
 });
 console.log(a);
+
+var a=[2,-3,4,-5]
+a=a.filter(function(x){
+    return x<-1
+});
+console.log(a);
+
+var a=[5,78,-67,98,-90]
+a=a.filter(function(x){
+    return x<-1
+})
+console.log(a)
+
+
+function mult(a,b=6){
+    return a*b;
+
+}
+console.log(mult(3,5))
